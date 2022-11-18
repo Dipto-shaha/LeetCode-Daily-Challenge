bc6bb900d83622a010c28defb1d201c81804955a
Dipto Shaha <64748257+Dipto-shaha@users.noreply.github.com>
2022-11-18 13:07:16 +0600

    Time: 5 ms (31.29%), Space: 5.9 MB (19.97%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        int x[]={2,3,5},i=0;
+        while(i<3)
+        {
+            if(n%x[i]==0) n/=x[i];
+            else i++;
+        }
+        if(n>2) return false;
+        return true;
+        
+    }
+};
