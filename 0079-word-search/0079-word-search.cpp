f8deeb88427bda5d3242c0831a8ab9fb2a0b51c8
Dipto Shaha <64748257+Dipto-shaha@users.noreply.github.com>
2022-11-24 12:09:56 +0600

    Time: 1627 ms (10.37%), Space: 8.1 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    vector<vector<char>>b;
-    bool fun(int i,int j,int l,string word)
+    bool fun(int i,int j,int l ,vector<vector<char>>& board,string word)
     {
         if(l==word.size()) return true;
-        if(i>=0 && i<b.size() && j>=0 && j<b[0].size() && b[i][j]==word[l])
-        {
-            char ch=b[i][j];
-            b[i][j]='.';
-            bool ans=false;
-            ans=ans||fun(i+1,j,l+1,word);
-            ans=ans||fun(i-1,j,l+1,word);
-            ans=ans||fun(i,j+1,l+1,word);
-            ans=ans||fun(i,j-1,l+1,word);
-            b[i][j]=ch;
-            return ans;
-
-        }
-        else 
-          return false;
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[l])
+            return false;
+        char ch=board[i][j];
+        board[i][j]='.';
+        bool ans=false;
+        ans=ans||fun(i+1,j,l+1,board,word);
+        ans=ans||fun(i-1,j,l+1,board,word);
+        ans=ans||fun(i,j+1,l+1,board,word);
+        ans=ans||fun(i,j-1,l+1,board,word);
+        board[i][j]=ch;
+        return ans;
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n=board.size(),m=board[0].size(),i,j;
-        b=board;
         for(i=0;i<n;i++)
         {
             for(j=0;j<m;j++)
             {
                 if(word[0]==board[i][j])
                 {
-                    if(fun(i,j,0,word))
+                    if(fun(i,j,0,board,word))
                         return true;
                 }
             }
