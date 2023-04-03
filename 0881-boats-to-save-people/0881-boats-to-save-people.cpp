8eeb289e4d03eb89ee43a5a47b5a1f3b98d59174
Dipto Shaha <64748257+Dipto-shaha@users.noreply.github.com>
2023-04-03 18:55:29 +0600

    Time: 83 ms (81.96%), Space: 41.9 MB (93.02%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -5,9 +5,7 @@ public:
         sort(p.begin(),p.end());
         while(i<=j)
         {
-          if(j>=1 && p[j]+p[j-1]<=l)
-              j-=2,ans++;
-           else if(p[i]+p[j]<=l)
+            if(p[i]+p[j]<=l)
                ans++,i++,j--;
            else ans++,j--;
         }
