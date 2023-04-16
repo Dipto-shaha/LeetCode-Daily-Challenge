d649891c37053fea3d2917643831fc676f62f508
Dipto Shaha <64748257+Dipto-shaha@users.noreply.github.com>
2023-04-16 23:37:01 +0600

    Time: 313 ms (54.31%), Space: 61.6 MB (37.50%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,18 +1,19 @@
-#define ll long long int
 class Solution {
 public:
-    vector<vector<ll>>cnt,dp;
+    vector<vector<int>>cnt,dp;
     int mod=1e9+7;
     int fun(int i,string &target,int j,int n)
     {
         if(i>=target.size()) 
+        { 
             return 1;
+        }
         if(j>=n) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        long long int ans1=0,ans2=0;
+        int ans1=0,ans2=0;
         if(cnt[j][target[i]-'a']) 
         {
-            ans1=(cnt[j][target[i]-'a']*fun(i+1,target,j+1,n))%mod;
+            ans1=(cnt[j][target[i]-'a']*1LL*fun(i+1,target,j+1,n))%mod;
         }
         ans2=fun(i,target,j+1,n);
         dp[i][j]=(ans1+ans2)%mod;
@@ -20,8 +21,8 @@ public:
     }
     int numWays(vector<string>& words, string target) {
         int n=words[0].size(),i;
-        cnt.resize(n,vector<ll>(26,0));
-        dp.resize(target.size(),vector<ll>(n,-1));
+        cnt.resize(n,vector<int>(26,0));
+        dp.resize(target.size(),vector<int>(n,-1));
         for(auto u:words)
         {
             for(i=0;i<n;i++)
