e91f2122b900475a01c0bdd4a6b011ff56be4e2d
Dipto Shaha <diptoshaha2635@gmail.com>
2024-03-08 13:12:11 +0600

    Time: 4 ms (68.91%), Space: 21.8 MB (20.46%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -4,10 +4,11 @@ public:
         int mx=0,ans=0,fq[101]={0};
         for(auto u:nums){
             fq[u]++;
-            mx=max(mx,fq[u]);
-        }
-        for(auto u:fq){
-            if(u==mx) ans++;
+            if(mx<fq[u]){
+                ans=1;
+                mx=fq[u];
+            }
+            else if(mx==fq[u]) ans++;
         }
         return ans*mx;
     }
