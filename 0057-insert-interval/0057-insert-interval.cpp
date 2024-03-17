a02fc37979fae189cdddee4206e16c2890525592
Dipto Shaha <diptoshaha2635@gmail.com>
2024-03-17 17:31:54 +0600

    Time: 10 ms (71.09%), Space: 20.5 MB (29.62%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n=intervals.size(),i,j,last=-1;
-        bool flag=true;
         vector<vector<int>>ans;
-        for(i=0;i<n;i++)
-        {
-            if(flag && intervals[i][0]>newInterval[0]  && intervals[i][0]>newInterval[1])
-            {
-                ans.push_back(newInterval);
+        int i,n=intervals.size();
+        for(i=0;i<n;i++){
+           if(intervals[i][0]< newInterval[0] && intervals[i][1] < newInterval[0])
+               ans.push_back(intervals[i]);
+            else if( newInterval[1] < intervals[i][0] &&  newInterval[1]< intervals[i][0] ){
+                if(newInterval[1]!=-1)
+                    ans.push_back(newInterval),newInterval[1]=-1;
                 ans.push_back(intervals[i]);
-                flag=false;
             }
-            else if(flag && newInterval[0]<=intervals[i][0] && intervals[i][0] <=newInterval[1])
-            {
-                intervals[i][0]=newInterval[0];
-                j=i;
-                while(j<n &&  newInterval[0]<=intervals[j][0] && intervals[j][0] <=newInterval[1])
-                {
-                    newInterval[1]=max(intervals[j][1],newInterval[1]);
-                    j++;
+            else{
+                int l=min(intervals[i][0],newInterval[0]),h=max(intervals[i][1],newInterval[1]);
+                while(i<n && intervals[i][0]<=h){
+                    h=max(intervals[i][1],h);
+                    i++;
                 }
-                intervals[i][1]=newInterval[1];
-                ans.push_back(intervals[i]);
-                flag=false;
-                i=j-1;
-
+                newInterval[1]=-1;
+                ans.push_back({l,h});
+                i--;
             }
-            else if(intervals[i][0]<=newInterval[0] && newInterval[0]<=intervals[i][1])
-            {
-                intervals[i][1]=max(intervals[i][1],newInterval[1]);
-                j=i+1;
-                while(j<n && intervals[i][1]>=intervals[j][0])
-                {
-                    intervals[i][1]=max(intervals[i][1],intervals[j][1]);
-                    j++;
-                }
-                ans.push_back(intervals[i]);
-                flag=false;
-                i=j-1;
-            }
-            else ans.push_back(intervals[i]);
         }
-        if(flag)ans.push_back(newInterval);
+        if(newInterval[1]!=-1) ans.push_back(newInterval);
         return ans;
     }
 };
