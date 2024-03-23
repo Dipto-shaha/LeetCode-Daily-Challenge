5826455937835c8f02505afc876a8efbeae31e68
Dipto Shaha <diptoshaha2635@gmail.com>
2024-03-23 12:16:48 +0600

    Time: 1585 ms (5.04%), Space: 21.3 MB (43.65%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* lastNode(ListNode* head) {
+        if (!head || !head->next) return head; 
+        while (head->next->next) {
+            head = head->next;
+        }
+        ListNode* last = head->next;
+        head->next = nullptr;
+        return last;
+    }
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        ListNode* temp = head;
+        ListNode* odd = head,*last;
+
+        while (temp->next) {
+            odd = temp->next; // odd points to the next node
+            last=lastNode(head);
+            temp->next = last; // breaking the last node from the first half
+            if (odd == last) break; // if odd is the last node, break
+            temp->next->next = odd; // pointing the next of the last node to odd (which is the next node)
+            temp = odd; // moving temp to the next node (odd)
+        }
+    }
+};
